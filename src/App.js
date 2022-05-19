e0c7d9db84a907dd07dbb063098aba846823710f
balbastrex <balbastrex@gmail.com>
2022-05-20 01:23:26 +0200

    upgraded scoreboard

--- src/App.js
+++ src/App.js
@@ -6,35 +6,45 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.topTitle}>
-          <Text style={styles.topTitleText}>DAM PADEL</Text>
+      <View style={styles.mainScreen}>
+        <View style={styles.topTitle}>
+            <Text style={styles.topTitleText}>DAM PADEL</Text>
+          </View>
+        <View style={styles.top}>
+          <View style={styles.topLeft}>
+            <Text style={styles.player1}>Paco Jonarte</Text>
+          </View>
+          <View style={styles.topRight}>
+            <Text style={styles.player2}>Rafa Navidad</Text>
         </View>
-      <View style={styles.top}>
-        <View style={styles.topLeft}>
-          <Text style={styles.player1}>Paco Jonarte</Text>
         </View>
-        <View style={styles.topRight}>
-          <Text style={styles.player2}>Rafa Navidad</Text>
-      </View>
-      </View>
-      <View style={styles.scoreboard}>
-        <View style={styles.scoreboardTop}>
-          <Text>Puntuación DAM PADEL</Text>
+        <View style={styles.scoreboard}>
+          <View style={styles.scoreboardTop}>
+          <View style={styles.scoreboardTop}><Text style={styles.scoreboardText}>DAW</Text></View>
+            <View style={styles.scoreboardTop}><Text style={styles.scoreboardText}>6</Text></View>
+            <View style={styles.scoreboardTop}><Text style={styles.scoreboardText}>3</Text></View>
+            <View style={styles.scoreboardTop}><Text style={styles.scoreboardText}>0</Text></View>
+            <View style={styles.scoreboardTop}><Text style={styles.scoreboardText}>40</Text></View>
+          </View>
+          <View style={styles.scoreboardBottom}>
+            <View style={styles.scoreboardBottom}><Text style={styles.scoreboardText}>DAW</Text></View>
+            <View style={styles.scoreboardBottom}><Text style={styles.scoreboardText}>4</Text></View>
+            <View style={styles.scoreboardBottom}><Text style={styles.scoreboardText}>5</Text></View>
+            <View style={styles.scoreboardBottom}><Text style={styles.scoreboardText}>0</Text></View>
+            <View style={styles.scoreboardBottom}><Text style={styles.scoreboardText}>15</Text></View>
+          </View>
         </View>
-        <View style={styles.scoreboardBottom}>
-          <Text>Puntuación DAW PADEL</Text>
+        <View style={styles.bottom}>
+          <View style={styles.bottomLeft}>
+            <Text style={styles.player3}>Belastegón</Text>
+          </View>
+          <View style={styles.bottomRight}>
+            <Text style={styles.player4}>Juan LeBrín</Text>
         </View>
-      </View>
-      <View style={styles.bottom}>
-        <View style={styles.bottomLeft}>
-          <Text style={styles.player3}>Belastegón</Text>
         </View>
-        <View style={styles.bottomRight}>
-          <Text style={styles.player4}>Juan LeBrín</Text>
-      </View>
-      </View>
-      <View style={styles.bottomTitle}>
-        <Text style={styles.bottomTitleText}>DAW PADEL</Text>
+        <View style={styles.bottomTitle}>
+          <Text style={styles.bottomTitleText}>DAW PADEL</Text>
+        </View>
       </View>
     </View>
   );
@@ -47,24 +57,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  topTitle: {
-    flex: 0.5,
+  mainScreen: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+  },
+  topTitle: {
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
     borderColor: '#DFFF4F',
     borderWidth: 2,
   },
   topTitleText: {
-    flex: 1,
-    borderColor: '#DFFF4F',
-    borderWidth: 2,
+    fontSize: 50,
     color: '#ffffff',
   },
   scoreboard: {
     flex: 0.5,
     alignItems: 'center',
     justifyContent: 'center',
-    borderColor: '#DFFF4F',
+    borderColor: '#0d0d0d',
     borderWidth: 2,
   },
   scoreboardTop: {
@@ -72,28 +86,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    borderColor: '#DFFF4F',
-    borderWidth: 2,
   },
   scoreboardBottom: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    borderColor: '#DFFF4F',
-    borderWidth: 2,
+  },
+  scoreboardText: {
+    fontSize: 30,
   },
   bottomTitle: {
-    flex: 0.5,
+    flex: 0.3,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     borderColor: '#DFFF4F',
     borderWidth: 2,
   },
   bottomTitleText: {
-    flex: 1,
-    borderColor:  '#DFFF4F',
-    borderWidth: 2,
+    fontSize: 50,
     color: '#ffffff',
   },
   top: {
