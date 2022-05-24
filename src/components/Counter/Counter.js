f43b15a24022476625bd14e78a857d21d392f908
balbastrex <balbastrex@gmail.com>
2022-05-24 02:33:51 +0200

    fixed counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,24 +1,21 @@
 import { React, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
 
 export default function Counter() {
     const [points, setPoints] = useState('0');
     const [control, setControl] = useState(0);
-
+    
     // let pointsTop= 0;
     //let control = 0;
 
-    const rule = ['0', '15', '30', '40'];
+    const rule = ['0','15','30','40'];
 
     const controlMore = () => {
 
         if (control < 3) {
             setControl(control + 1);
-
-            pointsUpdate(control);
-
-            // setPoints(rule[control + 1]);
-            // console.log(control);
+            setPoints(rule[control + 1]);
+            console.log(control);
         }
     };
 
@@ -26,17 +23,9 @@ export default function Counter() {
 
         if (control > 0) {
             setControl(control - 1);
-
-            pointsUpdate(control);
-
-            // setPoints(rule[control - 1]);
-            // console.log(control);
+            setPoints(rule[control - 1]);
+            console.log(control);
         }
-    };
-
-    const pointsUpdate = (control) => {
-        setPoints(rule[control]);
-        console.log(control);
     };
 
 
@@ -50,12 +39,12 @@ export default function Counter() {
                 <Text style={styles.buttonsText}>{points}</Text>
             </View>
             <View>
-                {/* <TouchableOpacity 
+            {/* <TouchableOpacity 
                 style={styles.buttons} 
                 onChangeText = {(points) => { setpoints(points) this.props.onChangeValue(value) }} 
                     onPress={() =>controlMore()}><Text style={styles.buttonsText}>+</Text></TouchableOpacity> */}
-                <TouchableOpacity style={styles.buttons} onPress={() => controlMore()}><Text style={styles.buttonsText}>+</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.buttons} onPress={() => controlLess()}><Text style={styles.buttonsText}>-</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.buttons} onPress={() =>controlMore()}><Text style={styles.buttonsText}>+</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.buttons} onPress={() =>controlLess()}><Text style={styles.buttonsText}>-</Text></TouchableOpacity>
             </View>
         </View >
     )
